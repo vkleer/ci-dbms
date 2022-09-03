22f6449321bdbaf92e2aeb5f7566f1c56433134e
Vilayat Kleer <vkleer@hotmail.com>
2022-09-03 23:00:21 +0000

    Add multiple queries to test in sql-psycopg2 script

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -7,7 +7,32 @@ connection = psycopg2.connect(database="chinook")
 cursor = connection.cursor()
 
 # Query 1 - select all records from the "Artist" table
-cursor.execute('SELECT * FROM "Artist"')
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name" columns from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only by "ArtistId" number 51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only the albums with "ArtistId" number 51 from the "Album" 
+# table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Track"
+# table
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# Query 7 - select only the albums with "ArtistId" number 8 from the "Album"
+# table
+cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [8])
+
+# Query 8 - select all tracks where the composer is "test" from the "Album"
+# table, which will return nothing
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["test"])
 
 # Fetch the results (multiple)
 results = cursor.fetchall()
